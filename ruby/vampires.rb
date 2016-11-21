1b05a2ff2e2210da9f6c7cfb569612bbd6b03ee3
Dylan Cordoni <dylancordoni@gmail.com>
2016-11-21 05:50:25 -0800

    Add: questionare

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+fullName = gets.chomp
+
+puts "How old are you?" 
+age = gets.chomp
+
+puts "What year were you born?"
+birthYear = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+
+puts "Would you like to enrool in the company's health insurance?"
+insurance = gets.chomp
