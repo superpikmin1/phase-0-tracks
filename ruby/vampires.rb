eeeea537e47fdba528b52e698d5f64ea5688f8a0
Dylan Cordoni <dylancordoni@gmail.com>
2016-11-21 06:38:11 -0800

    Add: multi-employees, allergy test and last comment

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,8 @@
+puts "How many employees will be processed?"
+employees = gets.chomp
+employees = employees.to_i
+counter = 0
+until counter == employees
 puts "What is your name?"
 fullName = gets.chomp
 
@@ -21,12 +26,22 @@ if age.to_i != (2016 - birthYear.to_i) && garlic == "n" || insurance == "n"
 	result = "Probably a vampire"
 end
 if age.to_i != (2016 - birthYear.to_i) && garlic == "n" && insurance == "n"
-	result = "Almost certainly a vampire"
+result = "Almost certainly a vampire"
 end
 if fullName == "Drake Cula" || fullName == "Tu Fang"
 	result = "Definitely a vampire"
 end
-
-print result
-		
+puts "Do you have any allergies? If not or once listed type 'done'"
+allergy = nil
+until allergy == "done"
+	allergy = gets.chomp
+	if allergy == "sunshine"
+		result = "Probably a vampire"
+		break
+	end
+end
+puts result
+counter+=1
+end
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
