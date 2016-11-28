548f1ae986dfed553c4bcd5a851f0c5b1ee0c261
Dylan Cordoni <dylancordoni@gmail.com>
2016-11-28 05:21:07 -0800

    Add: sorting method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -31,11 +31,37 @@ def fib (int)
 	fib_array
 end
 
-p fib(6)
-p fib(0)
-p fib(1)
-p fib(2)
+#p fib(6)
+#p fib(0)
+#p fib(1)
+#p fib(2)
+#trail = fib(100)
+#p trail[-1]
 
-trail = fib(100)
+#-define method that takes in a array of integers
+#	-compare first 2 indexes and have the smaller one come first
+#	-must switch values at specific indexes (use place holder variable)
+#	-if not sorted reset counter
+#	-if the first 2 indexes are sorted, move on the the 2nd and 3rd indexes
+#	-repeat
 
-p trail[-1]
+def sort_int (arr)
+	counter = 0
+	while counter + 1 < arr.length  
+		if arr[counter] <= arr[counter + 1]
+			counter+=1
+		elsif arr[counter] > arr[counter + 1]
+			place_holder = arr[counter]
+			arr[counter] = arr[counter + 1]
+			arr[counter + 1] = place_holder
+			counter = 0
+		end
+	end
+	arr
+end
+
+ray = [6, 2, 77, 1, 0, 9]
+
+
+
+p sort_int(ray)
