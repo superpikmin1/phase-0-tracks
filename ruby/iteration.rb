a66cf27f8b4ee918365984b601c93455174807f9
Dylan Cordoni <dylancordoni@gmail.com>
2016-11-27 23:15:36 -0800

    Add: filtering methods

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -23,8 +23,31 @@ fruits.map! do |fruit|
 	fruit.capitalize!
 end
 
-
-
 p fruits
 p vegetables
 
+#deletion
+#fruits.delete_if {|fruit| fruit.length > 6}
+#vegetables.delete_if {|veg, color| color == "green"}
+
+#p fruits
+#p vegetables
+
+#selection
+#fruits.select!{|fruit| fruit.length < 5}
+#vegetables.select!{|veg, color| color == "orange"}
+
+#p fruits
+#p vegetables
+
+#selection2
+#p fruits.bsearch{|fruit| fruit == "Strawberry"}
+#vegetables.keep_if{|veg, color| color == "orange"}
+
+#dropping
+#fruits = fruits.drop_while{|fruit| fruit.include? "a"}
+#vegetables.reject! {|veg, color| color != "green"}
+
+
+#p fruits
+#p vegetables
