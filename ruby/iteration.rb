4027213515887fbd31d76963cf8c7c892fa917d8
Dylan Cordoni <dylancordoni@gmail.com>
2016-11-27 22:23:23 -0800

    Add: .each method to array and hash, modified array with .map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -7,3 +7,24 @@ vegetables = {
 
 p fruits
 p vegetables
+
+fruits.each do |fruit|
+	puts fruit
+end
+
+vegetables.each do |vegetable, color|
+	puts "A #{vegetable} is #{color}"
+end
+
+p fruits
+p vegetables
+
+fruits.map! do |fruit|
+	fruit.capitalize!
+end
+
+
+
+p fruits
+p vegetables
+
