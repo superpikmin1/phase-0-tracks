f38733847079a812a7d1d4b25136483a4f2c8fdd
Dylan Cordoni <dylancordoni@gmail.com>
2016-12-03 18:23:21 -0800

    Add: instance variables assigned from initialization

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,7 +5,12 @@ class Santa
 	def eat_milk_and_cookies (cookie)
 		puts "That was a good #{cookie}"
 	end
-	def initialize
+	def initialize (gender, ethnicity, reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"],
+ age = 0)
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = reindeer_ranking
+		@age = age
 		puts "initializing Santa instance..."
 	end
 end 
