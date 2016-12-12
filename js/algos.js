c7b2381c87134082a71b9a743f0aab4367dc41fa
Dylan Cordoni <dylancordoni@gmail.com>
2016-12-12 07:01:33 -0800

    Add: random word generator and new driver code for 2 methods

--- js/algos.js
+++ js/algos.js
@@ -22,12 +22,25 @@ function pairing(obj1, obj2) {
 	return share;
 }
 
+function randomWords(int) {
+	var wordArray = [];
+	var alpha = "abcdefghijklmnopqrstuvwxyz";
+	for(var i = 0; i < int; i++){
+		var word = ""
+		for(var x = 0; x < Math.floor((Math.random() * 10) + 1); x++){
+			word = word + alpha[Math.floor((Math.random() * 25) + 0)];
+		}
+		wordArray.push(word);
+	}
+	return wordArray;
+}
 
-
+//for longest phrase function
 var phrases = ["This is a phrase", "This is also a phrase", "yessir"];
 var long = longestPhrase(phrases);
 console.log(long);
 
+//for pairing function
 var charmander = {color: 'red', height: 3};
 var charizard = {color: 'red', height: 7};
 var zapados = {color: 'yellow', height: 6};
@@ -35,3 +48,15 @@ var similar = pairing(charmander, charizard);
 console.log(similar);
 var different = pairing(zapados, charizard);
 console.log(different);
+
+//for random words function
+var randos = randomWords(3);
+console.log(randos);
+
+//final driver code
+for(var y = 0; y < 10; y++) {
+	var randomArray = randomWords(5);
+	console.log(randomArray);
+	var longWord = longestPhrase(randomArray);
+	console.log(longWord)
+}
