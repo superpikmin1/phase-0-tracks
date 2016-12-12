d92eb910acf61449082c1dd0e493da65bf49c860
Dylan Cordoni <dylancordoni@gmail.com>
2016-12-12 06:04:32 -0800

    Add: constructor and objects from it

--- js/data_structures.js
+++ js/data_structures.js
@@ -10,3 +10,20 @@ for(var i = 0; i < horses.length; i++) {
 	horseBook[name] = color;
 }
 console.log(horseBook)
+
+
+function Car (model, year, color, cool) {
+	this.model = model;
+	this.year = year;
+	this.color = color;
+	this.cool = cool;
+
+	this.vroom = function() {
+		console.log("VROOOOM")
+	}
+}
+
+var myCar = new Car("Kia Soul", 2013, "red", false);
+var jonsCar = new Car("Mazda5", 2003, "blue", false);
+var JimsCar = new Car("Tesla", 2015, "blue", true);
+console.log(myCar);
