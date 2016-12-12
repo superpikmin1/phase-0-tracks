825c7c5e2a11a25bd45a7a08d569c268597ccf7f
Dylan Cordoni <dylancordoni@gmail.com>
2016-12-12 05:54:29 -0800

    Add: another array combining the first two

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,12 @@
-colors = ["green", "yellow", "pink", "black"];
-horses = ["Sharon", "Kathy", "Silver", "Branco"];
+var colors = ['green', 'yellow', 'pink', 'black'];
+var horses = ['Sharon', 'Kathy', 'Silver', 'Branco'];
 
-colors.push("purple");
-horses.push("Jon");
+colors.push('purple');
+horses.push('Jon');
+var horseBook = {};
+for(var i = 0; i < horses.length; i++) {
+	var name = horses[i];
+	var color = colors[i];
+	horseBook[name] = color;
+}
+console.log(horseBook)
